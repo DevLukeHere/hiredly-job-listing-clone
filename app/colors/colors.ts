749fea30929620da9f1eb0849d887e8129bd1afb
Luke Wong <lukewong93@gmail.com>
2023-07-04 16:28:26 +0800

    refactor: setup project files

---
+++ app/colors/colors.ts
@@ -0,0 +1,9 @@
+const Colors = {
+  white: "#ffffff",
+  black: "#222222",
+  primary: "#512acc",
+  secondary: "#6ec197",
+  tertiary: "#f5f5f5",
+};
+
+export default Colors;
---
+++ app/jobs/page.tsx
@@ -0,0 +1,7 @@
+export default function Jobs() {
+  return (
+    <main>
+      <h1>This is the Hiredly jobs page.</h1>
+    </main>
+  );
+}
